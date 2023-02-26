25ff0f32e9979cefbc3989498ee12eb6d6df38ff
Andrey <alvice01andru@gmail.com>
2023-02-26 16:58:35 +0300

    Рефакторинг модуля api.js

--- src/js/api.js
+++ src/js/api.js
@@ -1,21 +1,16 @@
 import {toggleLoader} from './util.js';
 
-
-
-export async function getFilms(downloadPage, onSucces) {
+async function getFilms(numberPage) {
     toggleLoader();
     
-    const URL = `https://kinopoiskapiunofficial.tech/api/v2.2/films/top?page=${downloadPage}`;
+    const URL = `https://kinopoiskapiunofficial.tech/api/v2.2/films/top?page=${numberPage}`;
     const requestHeaders = new Headers({
         "accept": "application/json",
         "X-API-KEY": "07a46248-65ff-49d8-a29a-357fc89d387e",
     });
 
     try {
-        const request = await fetch(URL, {
-            headers: requestHeaders,
-        });
-
+        const request = await fetch(URL, { headers: requestHeaders });
         const result = await request.json();
 
         toggleLoader();
@@ -25,3 +20,5 @@ export async function getFilms(downloadPage, onSucces) {
         alert(err);
     }
 }
+
+export { getFilms };
